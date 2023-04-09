72849f5c99e93ad5568363b1fde5d096056e3fc2
Bartlomiej <bsotniczuk@gmail.com>
2023-04-09 23:01:18 +0200

    Version 0.0.1 (Proof of Concept with working REST API calls) -New app icon -New app name -Added labels for Temperature and Humidity -Added empty Arduino IDE file -Added REST API fetcher

--- app/src/main/java/pl/bsotniczuk/hygrosense/MainActivity.java
+++ app/src/main/java/pl/bsotniczuk/hygrosense/MainActivity.java
@@ -1,14 +1,58 @@
 package pl.bsotniczuk.hygrosense;
 
 import androidx.appcompat.app.AppCompatActivity;
+import pl.bsotniczuk.hygrosense.viewcontroller.MainActivityViewController;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ApiFetcher apiFetcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.apiFetcher = new ApiFetcher();
+
         setContentView(R.layout.activity_main);
+
+        MainActivityViewController mainActivityViewController = new MainActivityViewController(findViewById(R.id.temperatureValueTextView), findViewById(R.id.humidityValueTextView));
     }
+
+    public void getHumidity(View view) {
+        this.apiFetcher.fetchApiData();
+    }
+
+    public void getDataFromSensor(View view) {
+        this.apiFetcher.fetchApiDataInfo();
+    }
+
+    public void refreshTextViewThread() {
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                while (!isInterrupted()) {
+                    try {
+                        Thread.sleep(2000);  //1000ms = 1 sec
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+
+//                                temperatureValueTextView
+//                                textView1.setText(progressData);
+//                                textView2.setText(accelerometerDataOutput);
+//                                textView3.setText(gyroscopeDataOutput);
+                            }
+                        });
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        t.start();
+    }
+
 }
---
+++ app/src/main/java/pl/bsotniczuk/hygrosense/viewcontroller/MainActivityViewController.java
@@ -0,0 +1,12 @@
+package pl.bsotniczuk.hygrosense.viewcontroller;
+
+import android.widget.TextView;
+
+public class MainActivityViewController {
+
+    public MainActivityViewController(TextView temperatureValueTextView, TextView humidityValueTextView) {
+
+    }
+
+
+}
