8842e19a4f638ea9a96ccb205d321c2fdbdf00a9
Bartlomiej <bsotniczuk@gmail.com>
2023-05-01 21:30:33 +0200

    Version 0.0.2 -Working background thread refreshing remote data -Added standard and night theme -Added vector images (need to be aligned)

--- app/src/main/java/pl/bsotniczuk/hygrosense/MainActivity.java
+++ app/src/main/java/pl/bsotniczuk/hygrosense/MainActivity.java
@@ -17,7 +17,13 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        MainActivityViewController mainActivityViewController = new MainActivityViewController(findViewById(R.id.temperatureValueTextView), findViewById(R.id.humidityValueTextView));
+        MainActivityViewController mainActivityViewController =
+                new MainActivityViewController(this,
+                        this.apiFetcher,
+                        findViewById(R.id.temperatureValueTextView),
+                        findViewById(R.id.humidityValueTextView)
+                );
+        this.apiFetcher.addListener(mainActivityViewController);
     }
 
     public void getHumidity(View view) {
@@ -26,33 +32,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void getDataFromSensor(View view) {
         this.apiFetcher.fetchApiDataInfo();
-    }
-
-    public void refreshTextViewThread() {
-        Thread t = new Thread() {
-            @Override
-            public void run() {
-                while (!isInterrupted()) {
-                    try {
-                        Thread.sleep(2000);  //1000ms = 1 sec
-
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-
-//                                temperatureValueTextView
-//                                textView1.setText(progressData);
-//                                textView2.setText(accelerometerDataOutput);
-//                                textView3.setText(gyroscopeDataOutput);
-                            }
-                        });
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        };
-        t.start();
     }
 
 }
--- app/src/main/java/pl/bsotniczuk/hygrosense/viewcontroller/MainActivityViewController.java
+++ app/src/main/java/pl/bsotniczuk/hygrosense/viewcontroller/MainActivityViewController.java
@@ -1,12 +1,55 @@
 package pl.bsotniczuk.hygrosense.viewcontroller;
 
+import android.util.Log;
 import android.widget.TextView;
 
-public class MainActivityViewController {
+import pl.bsotniczuk.hygrosense.ApiFetcher;
+import pl.bsotniczuk.hygrosense.HygroEventListener;
+import pl.bsotniczuk.hygrosense.MainActivity;
 
-    public MainActivityViewController(TextView temperatureValueTextView, TextView humidityValueTextView) {
+public class MainActivityViewController implements HygroEventListener {
 
+    MainActivity mainActivity;
+    ApiFetcher apiFetcher;
+    TextView temperatureValueTextView;
+    TextView humidityValueTextView;
+
+    public MainActivityViewController(MainActivity mainActivity, ApiFetcher apiFetcher, TextView temperatureValueTextView, TextView humidityValueTextView) {
+        this.mainActivity = mainActivity;
+        this.apiFetcher = apiFetcher;
+        this.temperatureValueTextView = temperatureValueTextView;
+        this.humidityValueTextView = humidityValueTextView;
+
+        refreshTextViewThread();
     }
 
+    @Override
+    public void hygroDataChanged() {
+        Log.i("HygroSense", "hygro data changed");
+        temperatureValueTextView.setText("" + apiFetcher.getTemperature() + " \u2103");
+        humidityValueTextView.setText("" + apiFetcher.getHumidity() + " %");
+    }
+
+    public void refreshTextViewThread() {
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                while (!isInterrupted()) {
+                    try {
+                        Thread.sleep(3000);  //1000ms = 1 sec
+                        mainActivity.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                apiFetcher.fetchApiDataInfo();
+                            }
+                        });
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        t.start();
+    }
 
 }
