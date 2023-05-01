a2d6be38597a168ffb8816d41ab65d288c80deb6
Bartlomiej <bsotniczuk@gmail.com>
2023-05-01 21:30:33 +0200

    Version 0.0.2 -Working background thread refreshing remote data -Added standard and night theme -Added vector images (need to be aligned)

--- app/src/main/java/pl/bsotniczuk/hygrosense/ApiFetcher.java
+++ app/src/main/java/pl/bsotniczuk/hygrosense/ApiFetcher.java
@@ -4,6 +4,9 @@ import android.util.Log;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import pl.bsotniczuk.hygrosense.api.HumidityApi;
 import pl.bsotniczuk.hygrosense.api.SensorDataApi;
 import pl.bsotniczuk.hygrosense.model.SensorData;
@@ -15,23 +18,61 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiFetcher {
 
-    private static Float temperature = 0f;
-    private static Float humidity = 0f;
+    private Float temperature = 0f;
+    private Float humidity = 0f;
+    private List<HygroEventListener> listeners = new ArrayList<HygroEventListener>();
 
-    public static Float getTemperature() {
+    public void addListener(HygroEventListener toAdd) {
+        listeners.add(toAdd);
+    }
+
+    public Float getTemperature() {
         return temperature;
     }
 
-    public static void setTemperature(Float temperature) {
-        ApiFetcher.temperature = temperature;
+    public void setTemperature(Float temperature) {
+        this.temperature = temperature;
     }
 
-    public static Float getHumidity() {
+    public Float getHumidity() {
         return humidity;
     }
 
-    public static void setHumidity(Float humidity) {
-        ApiFetcher.humidity = humidity;
+    public void setHumidity(Float humidity) {
+        this.humidity = humidity;
+    }
+
+    public void fetchApiDataInfo() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.1.18/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        SensorDataApi sensorDataApi = retrofit.create(SensorDataApi.class);
+        Call<SensorData> call = sensorDataApi.getSensorData();
+
+        call.enqueue(new Callback<SensorData>() {
+            @Override
+            public void onResponse(Call<SensorData> call, Response<SensorData> response) {
+                if (response.code() == 200) { //HTTP Code 200 equals to OK
+
+                    setTemperature(response.body().getTemperature());
+                    setHumidity(response.body().getHumidity());
+                    String deviceName = response.body().getDeviceName();
+
+                    Log.i("HygroSense", "Data fetched, temp: " + getTemperature() +
+                            " | humidity: " + getHumidity() + " | deviceName: " + deviceName);
+
+                    for (HygroEventListener hl : listeners)
+                        hl.hygroDataChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<SensorData> call, Throwable t) {
+                Log.i("HygroSense", "Data call to API failed: " + t);
+            }
+        });
     }
 
     public void fetchApiData() {
@@ -56,45 +97,6 @@ public class ApiFetcher {
 
             @Override
             public void onFailure(Call<String> call, Throwable t) {
-                Log.i("HygroSense", "Data call to API failed: " + t);
-            }
-        });
-    }
-
-    public void fetchApiDataInfo() {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.1.18/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        SensorDataApi sensorDataApi = retrofit.create(SensorDataApi.class);
-        Call<SensorData> call = sensorDataApi.getSensorData();
-
-        call.enqueue(new Callback<SensorData>() {
-            @Override
-            public void onResponse(Call<SensorData> call, Response<SensorData> response) {
-                if (response.code() == 200) { //HTTP Code 200 equals to OK
-//                    Float temperature = response.body().getTemperature();
-//                    Float humidity = response.body().getHumidity();
-//                    temperature = response.body().getTemperature();
-//                    humidity = response.body().getHumidity();
-
-                    setTemperature(response.body().getTemperature());
-                    setHumidity(response.body().getHumidity());
-
-                    String deviceName = response.body().getDeviceName();
-//                    if (StringUtils.isNotBlank(jsonMessage)) {
-//                        Log.i("HygroSense", "Data fetched, temp: " + temperature +
-//                                " | humidity: " + humidity + " | deviceName: " + deviceName);
-
-                    Log.i("HygroSense", "Data fetched, temp: " + getTemperature() +
-                            " | humidity: " + getHumidity() + " | deviceName: " + deviceName);
-//                    }
-                }
-            }
-
-            @Override
-            public void onFailure(Call<SensorData> call, Throwable t) {
                 Log.i("HygroSense", "Data call to API failed: " + t);
             }
         });
---
+++ app/src/main/java/pl/bsotniczuk/hygrosense/HygroEventListener.java
@@ -0,0 +1,6 @@
+package pl.bsotniczuk.hygrosense;
+
+//Observer Pattern
+public interface HygroEventListener {
+    void hygroDataChanged();
+}
