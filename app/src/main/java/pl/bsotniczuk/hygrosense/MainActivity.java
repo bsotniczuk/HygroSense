7d9e88387b4d37b19438ffcfe2269146d8604deb
Bartlomiej <bsotniczuk@gmail.com>
2023-08-07 19:01:18 +0200

    Version 0.0.7 -Leftover bluetooth functionality deleted from SettingsActivity -Leftover debug buttons deleted -Deleted leftover code

--- app/src/main/java/pl/bsotniczuk/hygrosense/MainActivity.java
+++ app/src/main/java/pl/bsotniczuk/hygrosense/MainActivity.java
@@ -10,25 +10,10 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    //TODO: Test wifi connection based on settings
-    //TODO: encrypt wifi password in database and hide when entering in settings
-    //TODO: Bluetooth connection with esp32 to exchange data (send data, and receive if they were received correctly)
-    //TODO: add disconnected animation when the phone cannot get data from esp32 for 30 seconds
-
-    //TODO: save ESP32 ssid and password to EEPROM, restart, then load from EEPROM
-    //TODO: do an AWS core endpoint to be able to access the ESP32 from anywhere in the world
-    //TODO: access AWS endpoint from this app
-    //TODO: add POST that will be able to send ssid and password to ESP32 using its access point capabilities
-    //TODO: add accesspoint mode so it can also read data from 192.168.4.1
-    //TODO: HygroEventListener set for AwsIotCoreIntegration
-    //TODO: delete bluetooth related functionality
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
         new MainActivityViewController(this);
     }
 
